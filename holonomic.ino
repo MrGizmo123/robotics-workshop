fd70890e6087c2ce3d6589ce5ad3a3be7a1074b1
Hrushikesh Patwardhan <hrushikeshp13@gmail.com>
2024-11-22 08:58:39 +0530

    remove some code for fill in the blanks

--- holonomic.ino
+++ holonomic.ino
@@ -24,57 +24,21 @@
 volatile bool isOut = false;
 
 void setup() {
-	Serial.begin(115200);
+  /* Serial begin */
 
 	/* Initialize motor direction pins */
-	setup_motors();
 
-	/* Initialize Hall sensor and LED pins */
-	pinMode(HALL_SENSOR_PIN, INPUT);
-	pinMode(LED_PIN, OUTPUT);
-	digitalWrite(LED_PIN, LOW);
+	/* Initialize Hall sensor pins */
 	
 	/* initialize dabble and set Bluetooth Name */
-	Dabble.begin("Holonomic Hunter"); 
 }
 
 void loop() {
 	
-	if (!isOut && digitalRead(HALL_SENSOR_PIN))
-	{
-	  isOut = true;								/* bot is out */
-	  digitalWrite(LED_PIN, HIGH); /* led should glow */
-    Serial.println("OUT");
-
-    move_in_direction(0,0);
-	}
-
-	/* This function handles the communication between phone and
-	 * esp32 */
-	Dabble.processInput();
+  /* Add check for Hall Sensor Input */
 
 	/* Only move the bot if it is not out */
-	if (!isOut)
-	{
-		float scaling  = 255.0 / 7.0; 
-		int velx = -scaling * GamePad.getXaxisData();
-		int vely = scaling * GamePad.getYaxisData();
 
-    Serial.print("velx: ");
-    Serial.print(velx);
-    Serial.print(", vely: ");
-    Serial.println(vely);
-		
-		move_in_direction(velx, vely);
-	}
-
-	/* this condition is only for debugging purpose 
-	 * TODO: remove in final version */
-	if (GamePad.isStartPressed())
-	{
-		isOut = false;							/* bot is again in the game */
-		digitalWrite(LED_PIN, LOW);	/* turn led off */
-    Serial.println("NOT OUT");
-	}
+	/* reset the bot */
 	
 }
--- motor.ino
+++ motor.ino
@@ -17,14 +17,15 @@ void setup_motors()
 
 /* moves the bot in a given direction, x and y components of velocity must be given */
 void move_in_direction(int x, int y) {
-	
-  int val1 = ((float)(x + y))/1.414; /* activation of wheels 2 and 4 */
-  int val2 = ((float)(y - x))/1.414; /* activation of wheels 1 and 3 */
 
-  setMotor(M0_IN1, M0_IN2, M0_PWM, val1 * k0); /* top right */
-  setMotor(M1_IN1, M1_IN2, M1_PWM, val2 * k1); /* top left */
-  setMotor(M2_IN1, M2_IN2, M2_PWM, val2 * k2); /* bottom right */
-  setMotor(M3_IN1, M3_IN2, M3_PWM, val1 * k3); /* bottom left */
+	/* Find out the formulas */
+  int val1 = 0; /* activation of wheels 2 and 4 */
+  int val2 = 0; /* activation of wheels 1 and 3 */
+
+	setMotor(M0_IN1, M0_IN2, M0_PWM, val1); /* top right */
+  setMotor(M1_IN1, M1_IN2, M1_PWM, val2); /* top left */
+  setMotor(M2_IN1, M2_IN2, M2_PWM, val2); /* bottom right */
+  setMotor(M3_IN1, M3_IN2, M3_PWM, val1); /* bottom left */
 }
 
 // to set motor direction and speed using PWM
